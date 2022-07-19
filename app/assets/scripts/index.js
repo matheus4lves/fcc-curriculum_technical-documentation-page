4f1e7fd58954d4c602b485868c4fa3dc9a33c675
Matheus Alves <matheusalves.bmd@gmail.com>
2022-07-19 16:52:29 -0300

    Add some global styles
    Changes summary:
    
     - Add normalize.css
    
     - Set the font family of some elements

--- app/assets/scripts/index.js
+++ app/assets/scripts/index.js
@@ -1,4 +1,7 @@
 import "../styles/styles.css";
+import "@fontsource/source-sans-pro";
+import "@fontsource/source-code-pro";
+import "@fontsource/source-serif-pro";
 
 if (module.hot) {
   module.hot.accept();
